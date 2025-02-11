b7d65fcc10fc2f259c7bae17d666e5c1767e1509
Himanshu Suresh Chaudhari <himanshuchaudhari@Himanshus-MacBook-Air.local>
2025-02-11 16:32:56 +0530

    Removing comments

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,3 @@ const handler = NextAuth(authConfig)
 
 export { handler as GET, handler as POST }
 
-// console.log({
-//     clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-// })
